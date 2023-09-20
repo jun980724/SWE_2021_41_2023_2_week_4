52afa18528d3dba166ce0653098ebdce23e15e51
Lee heonjun <143599675+jun980724@users.noreply.github.com>
2023-09-20 20:05:01 +0900

    Add files via upload

---
+++ arrange.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+for file in files/*.txt; do
+    
+    first_letter=$(basename "$file" | cut -c1 | tr '[:upper:]' '[:lower:]')
+
+    
+    mkdir -p "$first_letter"
+
+    
+    mv "$file" "$first_letter/"
+done
+
+echo "all files is moved."
